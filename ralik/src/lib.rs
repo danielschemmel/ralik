a89ef8f26e86800a5c1c8ea6195d7206153bf772
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-06-11 19:57:30 +0200

    Add and unify entry functions

--- ralik/src/lib.rs
+++ ralik/src/lib.rs
@@ -1,13 +1,27 @@
 #![forbid(unsafe_code)]
 
+use proc_macro2::TokenStream;
+
 mod error;
 pub use error::RunError;
 
 mod eval;
+
 mod syntax;
+use syntax::ast;
+
 mod value;
+
+fn run_expression(expression: syntax::ast::Expression) -> Result<syntax::ast::Expression, RunError> {
+	Ok(expression)
+}
 
 pub fn run_str(source: &str) -> Result<syntax::ast::Expression, RunError> {
 	let expression = syn::parse_str::<syntax::ast::Expression>(source)?;
-	Ok(expression)
+	run_expression(expression)
+}
+
+pub fn run_tokens(source: TokenStream) -> Result<syntax::ast::Expression, RunError> {
+	let expression = syn::parse2::<syntax::ast::Expression>(source)?;
+	run_expression(expression)
 }
