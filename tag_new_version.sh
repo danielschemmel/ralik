908c77f6e54b2d299e6e76577c659a8912d01943
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-06-17 19:29:52 +0200

    Release 0.0.1-alpha.0
    ralik@0.0.1-alpha.0
    ralik-repl@0.0.1-alpha.0
    
    Generated by cargo-workspaces

---
+++ tag_new_version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+cargo clippy -- -D warnings
+cargo test --all-features
+git diff --exit-code  # check if unstaged changes exist
+git diff --cached --exit-code  # check if staged, uncommitted changes exist
+exec cargo workspaces publish
